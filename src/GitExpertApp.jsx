a0e6a793cb3e58530b23d823a4fa3ba23dcb9db6
Lestrades_ubu <Lestrades@gmail.com>
2022-10-27 18:48:58 +0200

    Añadir nueva categoria

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 export const GitExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'Love', 'war', ]);
+
+    const onAddCategory = () => {
+        //Notas categories.push( "light" ); En React no se debe utilizar
+        setCategories([ ...categories, 'Light' ]);
+        //NotaOtraformaDeHacerlo   setCategories(cat => [...cat,'Light']);
+        //Borrar pruebas 
+        console.log('nueva categoria');
+        //Borrar pruebas 
+
+    }
+
     return (    
         <>
+            {/*Notas título */ }
             <h1>GitExpertApp</h1>
+            {/*Input */ }
+
+            {/*Listado de Gif */ }
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                { categories.map(category => {
+                    return <li key={ category }>{ category }</li>;
+                }) }
+
+            </ol>
+            {/*Gif Item */ }
         </>
     )
 }
