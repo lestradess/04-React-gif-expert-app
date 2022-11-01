0dacedbb837c3c41c1e141266062a9f47e13af51
Lestrades_ubu <Lestrades@gmail.com>
2022-11-01 19:57:57 +0100

    Cambios para segir con la rama master

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -19,7 +19,7 @@ export const GitExpertApp = () => {
 
 
     }
-
+    
     return (
         <>
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //Notas He quitado el StricMode pero se deviera trabajar con el activado en desarrollo
+  
   // <React.StrictMode>
   <GitExpertApp />
   //</React.StrictMode>
